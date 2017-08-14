b83d758bd0a9ed071336a1f58d6291ce7e45be4a
user <user@MacBook-Pro-13.local>
2017-08-14 23:02:49 +0300

    исправление ошибок

--- js/stat.js
+++ js/stat.js
@@ -1,36 +1,59 @@
+
 window.renderStatistics = function(ctx, names, times) {
+  console.log('Hello, world!');
+  var names = ['Вы', 'Катя', 'Игорь', 'Кекс'];
+  var times = [];
+
+  var fild = {
+    coords: [100, 10],
+    width: 420,
+    height: 270,
+
+  };
+
   ctx.fillStyle='rgba(0, 0, 0, 0.7)';
-  ctx.fillRect (110, 20, 420, 270);
+  ctx.fillRect (fild.coords[0] + 10, fild.coords[1] + 10, fild.width, fild.height);
   ctx.fillStyle="#FFFFFF";
-  ctx.fillRect (100, 10, 420, 270);
+  ctx.fillRect (fild.coords[0], fild.coords[1], fild.width, fild.height);
   ctx.fillStyle="#000000";
   ctx.font='16px PT Mono';
   ctx.fillText ('Ура вы победили!', 120, 40);
   ctx.fillText ('Список результатов:', 120, 60);
 
-  var names = ['Вы', 'Катя', 'Игорь', 'Кекс'];
-  var times;
-  names.length = times.length;
-
-  var max = -1;
-  var maxIndex = -1;
-  for(var i = 0 ; i < times.length; i ++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-      maxIndex = i;
+  for(var i = 0 ; i < names.length; i ++) {
+    if (names.length !== times.length) {
+        times.push(i + 1000);
     }
   }
 
-  var histigramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  console.log(times);
 
-  var barWidth = 40;
-  var indent = 50;
+  var histogram = {
+    coords: [140, 90],
+    width: 40,
+    height: 150,
+    indent: 100,
+    color_my: 'rgba(255, 0, 0, 1)', //мой цвет
+    color_they: 'rgba(23, 44, 251, 1)', //цвет других игроков
+  }
 
-  ctx.fillStyle='rgba(255, 0, 0, 1)';
+  for(var i = 0 ; i < names.length ; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = histogram.color_my;
+    } else {
+      ctx.fillStyle = histogram.color_they;
+    }
+
+    ctx.fillRect (histogram.coords[0], histogram.coords[1], histogram.width, histogram.height + (times[i] / 100));
+    ctx.fillText (times[i], histogram.coords[0], histogram.coords[1] - 15);
+    ctx.fillText (names[i], histogram.coords[0], histogram.coords[1] + histogram.height + (times[i] / 100) + 20);
+    histogram.coords[0] += histogram.indent;
+
+  }
 
 };
+
+
 
 
 
